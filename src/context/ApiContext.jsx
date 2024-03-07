57fd4d16c471e375faa32548b0a080b60169dcd9
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-03-07 18:52:33 +0530

    update context

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,15 +1,12 @@
 import { createContext,useState, useEffect } from "react"
 import axios from "axios"
-import useForm from './../hooks/useForm';
 
 const apikey = import.meta.env.VITE_API_KEY;
 
 export const ApiContext = createContext();
 
 export const ApiProvider = ({children}) =>{
-    //for handle search and search data
-    const [query, setQuery] = useForm('')
-    const [searchData, setSearchData] = useState()
+    const [searchData, setSearchData] = useState([])
 
     // for handle other api requests e.g. random recipe, homescreen recipe
     const [randomData, setRandom] = useState()
@@ -42,20 +39,19 @@ export const ApiProvider = ({children}) =>{
         },[])
 
     //for searching recipe 
-const searchRecipe = () => {
-        async function FetchData(){
-            try{
-                const res = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${apikey}`)
-                const data = res.data;
-                console.log(res.data.results);
-                setSearchData(data.results);
-            }catch(e){
-                console.log(e.response.data.message)
-            }}
-            FetchData();
+    const searchRecipe = async () => {
+        try {
+          const response = await axios.get(
+            `https://api.spoonacular.com/recipes/complexSearch?query=${searchData}&apiKey=${apikey}`
+          );
+          console.log(response.data.results);
+          setSearchData(response.data.results);
+        } catch (e) {
+          console.log("sorry item is not available");
         }
+      };
 
-    const values= {randomData,bulkRecipe,searchData,query,setQuery,setSearchData,searchRecipe}
+    const values= {randomData,bulkRecipe,searchData,setSearchData,searchRecipe}
     return(
         <ApiContext.Provider value={values}>
             {children}
