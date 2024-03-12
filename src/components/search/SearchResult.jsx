5975b9b96cd04eac9fade2e77e4d0f5833f89a63
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-03-12 11:35:32 +0530

    commit - make changes/implement details page

--- src/components/search/SearchResult.jsx
+++ src/components/search/SearchResult.jsx
@@ -1,12 +1,12 @@
 const SearchResult = ({searchProduct}) => {
   return (
     <div>
-        {searchProduct?.map(dt => (
+        {/* {searchProduct?.map(dt => (
             <div key={dt.id}>
                 <img src={dt.image}/>
                 <li >{dt.title}</li>
             </div>
-        ))}
+        ))} */}
         
     </div>
   )
