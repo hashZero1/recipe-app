74f7761b0335b75e3079b8b76ad8b39325cef3ea
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-03-14 12:37:32 +0530

    commit - add AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,16 @@ import './index.css'
 
 import { BrowserRouter } from 'react-router-dom'
 import { ApiProvider } from './context/ApiContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
     <BrowserRouter>
+    <AuthProvider>
     <ApiProvider>
       <App />
     </ApiProvider>
+    </AuthProvider>
     </BrowserRouter>
     
-  </React.StrictMode>,
 )
