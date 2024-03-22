5c17446cde5fccafdeadaff6c61c421d28974222
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-03-22 14:40:38 +0530

    commit - UI

--- src/components/home/DetailsPageComponent.jsx
+++ src/components/home/DetailsPageComponent.jsx
@@ -13,29 +13,45 @@ export default function DetailsPageComponent({recipe}) {
           <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0">
             <div className="p-4 bg-white overflow-y-scroll no-scrollbar rounded-t-[10px] flex-1">
               <div className="mx-auto w-14 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
-              <div className="flex flex-col justify-center px-4 mx-auto">
-                <Drawer.Title className="font-bold text-2xl mb-4">
+              <div className="flex flex-col bg-gray-100 w-11/12 justify-center p-10 mx-auto rounded-t-[20px]">
+                <Drawer.Title className="font-bold text-2xl lg:text-4xl mb-5  bg-white rounded-[12px]  p-4">
                   {recipe.title}
                 </Drawer.Title>
-                <div className="">
-                  <img src={recipe.image} alt={recipe.title}/>
-                  <p className="text-zinc-600 mt-5">
-                    <span className=" font-semibold">Total servings</span> - {recipe.servings}
-                </p>
-                <p className="text-zinc-600 mt-5 capitalize">
-                    <span className=" font-semibold">DishTypes</span> - {recipe.dishTypes[0]}, {recipe.dishTypes[1]}, {recipe.dishTypes[2]}, {recipe.dishTypes[3]}, {recipe.dishTypes[4]}
-                </p>
+                <div className="flex flex-col lg:flex-row justify-between lg:text-lg lg:w-3/5">
+                  <img className="rounded-[12px]" src={recipe.image} alt={recipe.title}/>
+                  <div className="my-4 lg:my-0">
+                  <b className="text-zinc-600 mt-5">
+                  Total servings 
+                </b>
+                <p className=" ">{recipe.servings}</p>
+                  </div>
+                 <div>
+                 <b className="text-zinc-600 mt-5 capitalize">
+                DishTypes 
+                </b>
+                <p className="capitalize">
+                  {recipe.dishTypes[0]}<br/> 
+                  {recipe.dishTypes[1]}<br/> 
+                  {recipe.dishTypes[2]}<br/> 
+                  {recipe.dishTypes[3]}<br/> 
+                  {recipe.dishTypes[4]}</p> 
+                 </div>
+              
                 </div>
-                <p className="text-zinc-600 mt-8">
-                <span className="text-xl font-semibold">Instructions</span> - {recipe.instructions}
-                </p>
-                <p className="text-zinc-600 mt-8">
-                <span className="text-xl font-semibold">Step by Steps Intruction</span><br/>
-                {anslyzedIns?.map(dt => <li key={dt.number}>{dt.step}</li>)}
-                </p>                
+                <div className=" lg:w-5/6">
+                <h2 className="font-semibold mb-2 text-2xl text-zinc-600 mt-8">
+                Instructions :
+                </h2>
+                <p className="text-lg">{recipe.instructions}</p>
+                <h3 className="mb-2 text-zinc-600 mt-8 text-xl font-semibold">
+                Step by Steps Intructions :
+                </h3>     
+                <p className="text-lg"> {anslyzedIns?.map(dt => <li key={dt.number}>{dt.step}</li>)}</p>
+                </div>
+                           
               </div>
             </div>
-            <div className="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
+            {/* <div className="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
               <div className="flex gap-6 justify-end max-w-md mx-auto">
                 <a
                   className="text-xs text-zinc-600 flex items-center gap-0.25"
@@ -54,7 +70,7 @@ export default function DetailsPageComponent({recipe}) {
 
                 </a>
               </div>
-            </div>
+            </div> */}
           </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
