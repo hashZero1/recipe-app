2ced68b21c055e9a5c12c63d7af0e6f110b2fb53
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-03-28 14:21:18 +0530

    commit

--- src/components/home/TrendingSection.jsx
+++ src/components/home/TrendingSection.jsx
@@ -1,17 +1,18 @@
-import React, { useContext} from 'react'
+import { useContext} from 'react'
 import { Tab } from '@headlessui/react'
 import RecipeComponent from './RecipeComponent'
 import { ApiContext } from '../../context/ApiContext';
 
 const TrendingSection = () => {
     const {bulkRecipe,randomData} = useContext(ApiContext);
+    console.log(`from Trending ${randomData}`)
 
   return (
       <Tab.Group>
-      <Tab.List className="relative p-10 flex py-5 items-center text-xl mt-4 text-gray-50">
-      <div className="flex-grow border-t border-gray-100"></div>  
-        <Tab className='bg-opacity-50 bg-white m-2'>Trending Recipe</Tab>
-        <Tab className='bg-opacity-50 bg-white m-2'>Most Searched Recipe</Tab>
+      <Tab.List className="relative mx-2 lg:mx-0 mt-10 lg:p-10 flex lg:py-5 items-center lg:text-xl lg:mt-4 text-gray-50">
+      <div className="flex-grow border-t border-gray-100 "></div>  
+        <Tab className='bg-opacity-50 bg-white m-2 focus:bg-red-500'>Trending Recipe</Tab>
+        <Tab className='bg-opacity-50 bg-white m-2 focus:bg-red-500'>Most Searched Recipe</Tab>
         <div className="flex-grow border-t border-gray-100"></div>
       </Tab.List>
       <Tab.Panels>
