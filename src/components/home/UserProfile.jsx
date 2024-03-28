5989c84af0a68705c5a44e3d6bf641d325a8d826
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-03-28 14:59:50 +0530

    commit - changes in dropdown menu

--- src/components/home/UserProfile.jsx
+++ src/components/home/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { Avatar, Dropdown } from "flowbite-react";
@@ -8,18 +8,18 @@ export default function UserProfile({ signOutHandler }) {
 
   const reload = () => window.location.reload();
   return (
-    <div className="bg-opacity-40 bg-white ml-2 rounded-md hover:bg-slate-50">
-      <Dropdown inline label={<Avatar img={currentUser.photoURL} rounded />}>
+    <div className="bg-opacity-40  bg-white ml-2 rounded-md hover:bg-slate-50">
+      <Dropdown inline label={<Avatar className="h-0 md:h-4 lg:w-10 lg:h-7" img={currentUser.photoURL} rounded />}>
         <Dropdown.Header>
           <span className="block text-gray-600 text-sm font-semibold">
             {currentUser.displayName}
           </span>
-          <span className="block text-gray-400 truncate text-sm font-medium">
+          <span className="block text-gray-500 truncate text-sm font-medium">
             {currentUser.email}
           </span>
         </Dropdown.Header>
         <Link
-          className="block px-4 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+          className="block m-2 rounded-md px-4 py-2 text-sm font-semibold bg-gray-500 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
           onClick={() => {
             signOutHandler();
             reload();
