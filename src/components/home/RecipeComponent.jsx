d8121a9244cfc1ae6e5a026abc28a3f3d46d36d2
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-02 14:55:12 +0530

    commit - implement add to favourite button

--- src/components/home/RecipeComponent.jsx
+++ src/components/home/RecipeComponent.jsx
@@ -1,9 +1,9 @@
-import {useContext } from 'react'
-import { ApiContext } from '../../context/ApiContext';
 import { ToastContainer } from "react-toastify";
 import { motion } from "framer-motion";
 import DetailsPageComponent from './DetailsPageComponent';
-import { Link } from 'react-router-dom';
+import { CartContext } from "../../context/CartContext";
+import { useContext } from "react";
+
 
 const time = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline-block">
 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -11,12 +11,30 @@ const time = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24
 
 
 const RecipeComponent = ({bulkRecipe}) => {
+  const { addItemToCart, notify } = useContext(CartContext);
+
     return (
     <section className='mt-10'>
         <div className='flex flex-wrap justify-center'>
         {bulkRecipe?.map(dt => (
              <motion.div whileHover={{ scale: 1.02 }} key={dt.id} className="p-2">
-             <div className="max-w-[20rem] m-2 lg:m-0 2xl:m-2 bg-opacity-50 bg-white rounded-lg shadow ">
+             <div className="relative max-w-[20rem] m-2 lg:m-0 2xl:m-2 bg-opacity-50 bg-white rounded-lg shadow ">
+             <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  onClick={() => {
+                    addItemToCart(dt);
+                    notify();
+                  }}
+                  className="absolute h-10 w-10 m-2 top-0 right-0 text-gray-50 p-2 bg-red-600 text-red hover:bg-gray-800 hover:text-white transition-all rounded-full cursor-pointer "
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                    clipRule="evenodd"
+                  />
+                </svg>
                <div className="p-4 rounded-t-lg bg-white">
                  <img
                    className="w-full h-48 object-contain"
@@ -39,13 +57,14 @@ const RecipeComponent = ({bulkRecipe}) => {
                    >
                      <DetailsPageComponent recipe={dt} />
                    </button>
+                   
                  </div>
                </div>
              </div>
            </motion.div>
         ))}
-        </div>
-      
+         <ToastContainer/>
+        </div>  
     </section>
   )
 }
