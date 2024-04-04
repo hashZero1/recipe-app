b5af4499d47abf57d48d15aea63f6aec4c36b2b5
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-04 12:43:01 +0530

    commit - revamped interface

--- src/components/home/CartComponent.jsx
+++ src/components/home/CartComponent.jsx
@@ -3,18 +3,18 @@ import { CartContext } from "../../context/CartContext";
 import uuid from "react-uuid";
 import { motion as m } from "framer-motion";
 import { AuthContext } from "../../context/AuthContext";
+import DetailsPageComponent from "./DetailsPageComponent";
 
 export const Cart = ({ toggle, handleToggle }) => {
-  const { cartItems, deleteItemCart, addItemToCart, removeItemToCart } =
-    useContext(CartContext);
+  const { cartItems, deleteItemCart } = useContext(CartContext);
   const { currentUser } = useContext(AuthContext);
-  console.log(cartItems)
+
   return (
     <m.aside
       initial={{ opacity: 0, x: 200 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: 200 }}
-      className="fixed w-screen h-screen -top-10 -right-5 lg:h-[90vh] lg:top-10 lg:left-[45em] 2xl:left-[79em] overflow-y-scroll no-scrollbar"
+      className="fixed w-screen h-screen z-20 -top-10 -right-5 lg:h-[90vh] lg:top-10 lg:left-[45em] 2xl:left-[79em] overflow-y-scroll no-scrollbar"
     >
       <div className="relative">
         <div className="m-10 mt-10 p-4 left-0 absolute xl:mt-20 xl:absolute xl:-top-20 xl:left-[190px] xl:w-[400px] bg-red-100 bg-opacity-90 rounded-br-xl rounded-bl-xl transition-all shadow-md">
@@ -49,59 +49,41 @@ export const Cart = ({ toggle, handleToggle }) => {
             </div>
           ) : (
             <div className="pt-10 w-[74vw] xl:w-full">
-              <div className="">
-                {/* <Checkout /> */}
-              </div>
               {cartItems.map((item) => {
-                const { id, title, quantity, images, price } = item;
-                const totalPrice = price * quantity;
+                const { id, title, image } = item;
 
                 return (
-                  <h1 key={id}>{title}</h1>
-                  // <div
-                  //   className="flex flex-col mx-2 my-4 p-2 bg-red-200 shadow-md rounded-xl"
-                  //   key={id}
-                  // >
-                  //   <div className="flex">
-                  //     <div className="m-4 w-64 object-contain">
-                  //       <img
-                  //         className="w-full rounded-xl object-fill"
-                  //         src={images[0]}
-                  //         alt={images}
-                  //       />
-                  //     </div>
-                  //     <div className="mt-5 w-72">
-                  //       <h3 className="mb-2 text-xl font-semibold">{title}</h3>
-                  //       <p className="mb-1">Eligible for FREE Shipping</p>
-                  //       <b>₹{totalPrice}</b>
-                  //     </div>
-                  //   </div>
-                  //   <div className="m-4 flex justify-evenly">
-                  //     <div className="flex flex-row bg-red-600 rounded-xl ">
-                  //       <button
-                  //         onClick={() => addItemToCart(item)}
-                  //         className=" bg-red-600 text-white hover:text-red-700 hover:bg-gray-100 w-12 rounded-l cursor-pointer"
-                  //       >
-                  //         <span className="m-auto text-2xl font-thin">+</span>
-                  //       </button>
-                  //       <span className="py-2 px-5 text-black font-semibold  bg-white">
-                  //         {quantity}
-                  //       </span>
-                  //       <button
-                  //         onClick={() => removeItemToCart(item)}
-                  //         className="bg-red-600 text-white hover:text-red-700 hover:bg-gray-100 w-12 rounded-r cursor-pointer"
-                  //       >
-                  //         <span className="m-auto text-2xl font-thin">-</span>
-                  //       </button>
-                  //     </div>
-                  //     <button
-                  //       onClick={() => deleteItemCart(item)}
-                  //       className="px-5 bg-black text-white font-semibold hover:bg-gray-100 hover:text-black rounded-xl"
-                  //     >
-                  //       Delete
-                  //     </button>
-                  //   </div>
-                  // </div>
+                  <div
+                    className="flex flex-col mx-2 my-4 p-2 bg-red-200 shadow-md rounded-xl"
+                    key={id}
+                  >
+                    <div className="flex flex-col-reverse ">
+                      <div className="w-40 ">
+                        <img
+                          className="rounded-xl object-fill"
+                          src={image}
+                          alt={image}
+                        />
+                      </div>
+                      <div className="my-2 w-full">
+                        <h3 className="mb-2 text-xl font-semibold">{title}</h3>
+                      </div>
+                    </div>
+                    <div className="m-4 flex justify-evenly">
+                      <button
+                        className=" w-full p-0 bg-red-600 text-white font-semibold hover:bg-gray-50 hover:text-black rounded-xl hover:border-0"
+                      >
+                         <DetailsPageComponent recipe={item}/>
+                      </button>
+                     
+                      <button
+                        onClick={() => deleteItemCart(item)}
+                        className="ml-4 w-full bg-gray-50 text-black font-semibold hover:bg-red-600 hover:text-white rounded-xl hover:border-0"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
                 );
               })}
             </div>
