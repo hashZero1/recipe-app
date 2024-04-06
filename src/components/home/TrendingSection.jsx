4affe47daff23d1edf8b8a921e5f38d23ef9efb7
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-06 18:00:10 +0530

    commit - ui changes

--- src/components/home/TrendingSection.jsx
+++ src/components/home/TrendingSection.jsx
@@ -3,6 +3,13 @@ import { Tab } from '@headlessui/react'
 import RecipeComponent from './RecipeComponent'
 import { ApiContext } from '../../context/ApiContext';
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
+const headings = ["Trending Recipe", "Most Searched Recipe"]
+
 const TrendingSection = () => {
     const {bulkRecipe,randomData} = useContext(ApiContext);
     console.log(`from Trending ${randomData}`)
@@ -10,10 +17,28 @@ const TrendingSection = () => {
   return (
       <Tab.Group>
       <Tab.List className="relative mx-2 lg:mx-0 mt-10 lg:p-10 flex lg:py-5 items-center lg:text-xl lg:mt-4 text-gray-50">
-      <div className="flex-grow border-t border-gray-100 "></div>  
-        <Tab className='bg-opacity-50 bg-white m-2 focus:bg-red-500'>Trending Recipe</Tab>
-        <Tab className='bg-opacity-50 bg-white m-2 focus:bg-red-500'>Most Searched Recipe</Tab>
-        <div className="flex-grow border-t border-gray-100"></div>
+      <div className="flex border-t border-gray-100 ">
+        </div>  
+        <div className='mt-10 w-full flex justify-center'>
+        {headings.map((category) => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames(
+                  'w-1/3 rounded-lg m-2  py-4 text-md font-medium leading-5',
+                  'bg-black ',
+                  selected
+                    ? 'bg-red-600 text-gray-100  border-0 shadow'
+                    : 'text-blue-100  hover:bg-white opacity-85 border-0 hover:text-gray-800'
+                )
+              }
+            >
+              {category}
+            </Tab>
+          ))}
+        </div>
+     
+    
       </Tab.List>
       <Tab.Panels>
         <Tab.Panel><RecipeComponent bulkRecipe={bulkRecipe}/></Tab.Panel>
