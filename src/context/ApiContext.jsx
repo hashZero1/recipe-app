637dfe9fa6df1e32816be721422ed5f284e4a451
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-11 18:46:36 +0530

    Add files via upload

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,4 +1,4 @@
-import { createContext,useState, useEffect } from "react"
+import { createContext,useState, useEffect, useCallback, useMemo } from "react"
 import axios from "axios";
 
 const apikey = import.meta.env.VITE_API_KEY;
@@ -7,26 +7,36 @@ export const ApiContext = createContext();
 
 export const ApiProvider = ({children}) =>{
     const [searchData, setSearchData] = useState([])
-
-    // for handle other api requests e.g. random recipe, homescreen recipe
+    const [randomRecipe, setRandomRecipe] = useState([])
+    const [randomNumber, setRandomNumber] = useState()
+// for handle other api requests e.g. random recipe, homescreen recipe
     const [randomData, setRandom] = useState()
     const [bulkRecipe, setBulkRecipe] = useState()
 
+    
+    // const nums = new Set();
+    // while(nums.size !== 8) {
+    //     nums.add(Math.floor(Math.random() * 1100) + 400);
+    // }
+    // console.log([...nums])
 
-    //for random recipe 
-    useEffect(() => {
-        async function FetchData(){
+
+//for random recipes to display on home screen
+    const randomFetch =  useCallback(async() => {
             try{
                 const res = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${apikey}&number=10`)
                 const data = res.data;
                 setRandom(data.recipes);
             }catch(e){
                 console.log(e.response.data.message)
-            }}
-            FetchData();
+            }
         },[])
+        
+    useEffect(() =>{
+        randomFetch()
+    },[randomFetch])
 
-    // for home screen recipe 
+// for home screen recipes  
     useEffect(() => {
         async function FetchData(){
             try{
@@ -39,8 +49,8 @@ export const ApiProvider = ({children}) =>{
             FetchData();
         },[])
 
-    //for searching recipe 
-    const searchRecipe = async () => {
+//for searching recipe  
+    const searchRecipe =  async() => {
         try {
           const response = await axios.get(
             `https://api.spoonacular.com/recipes/complexSearch?query=${searchData}&apiKey=${apikey}&addRecipeInformation=true`
@@ -51,8 +61,44 @@ export const ApiProvider = ({children}) =>{
           console.log("sorry item is not available");
         }
       };
+    
 
-    const values= {randomData,bulkRecipe,searchData,setSearchData,searchRecipe}
+ //give random recipe on click 
+    useEffect(() => {
+      async function randomNum(){
+        const number = Math.floor(Math.random() * 50000) + 1000;
+        setRandomNumber(number)
+        console.log(number)
+      }
+      randomNum();
+      },[])
+
+      useEffect(() => {
+        async function randomSearch(){
+          try {
+            const response = await axios.get(
+              `https://api.spoonacular.com/recipes/${randomNumber}/information?apiKey=${apikey}`
+            );  
+            console.log(response.data)
+            setRandomRecipe(response.data);
+          } catch (e) {
+            console.log("sorry item is not available");
+          }
+        }
+        randomSearch()
+      },[randomNumber])
+
+
+    const values= {
+        randomData,
+        bulkRecipe,
+        searchData,
+        setSearchData,
+        searchRecipe,
+        randomRecipe,
+        setRandomRecipe
+    }
+
     return(
         <ApiContext.Provider value={values}>
             {children}
