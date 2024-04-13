661e2885f1058df76e87c278b108444677f419ad
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-13 12:17:07 +0530

    commit - Hero component

--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,15 +1,15 @@
 import FooterComponent from './FooterComponent';
-import SearchComponent from '../search/SearchComponent';
 import NavComponent from './NavComponent';
 import TrendingSection from './TrendingSection';
 import RandomizedRecipe from './RandomizedRecipe';
+import HeroComponent from './HeroComponent';
 
 const HomeComponent = () => {
  
   return (
     <main className='bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-red-500 via-red-400 to-gray-100'>
     <NavComponent/>
-    <SearchComponent/>
+    <HeroComponent/>
     <TrendingSection/>
     <RandomizedRecipe/>
     <FooterComponent/>
