c1be4e86609003be8caec1105008acc0f927ba9d
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-13 12:16:38 +0530

    commit - changes in component names

---
+++ src/components/home/HeroComponent.jsx
@@ -0,0 +1,15 @@
+import SearchSuggestions from "../search/SearchSuggestionComponent";
+
+const HeroComponent = () => {
+  return (
+  <section className="mt-24">
+    <div className="my-10 py-10 ">
+      <h1 className="text-center text-5xl text-white font-bold uppercase m-6">Hey there! 👀</h1>
+      <h2 className="text-center text-2xl lg:text-3xl text-white">What are you planning on making today? 😋</h2>
+    </div>
+    <SearchSuggestions/>
+  </section>
+  );
+};
+
+export default HeroComponent;
