39eb7d274ba59b7990758e20d3930279de794991
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-16 15:05:54 +0530

    commit - add cloudinary image

--- src/components/home/FooterComponent.jsx
+++ src/components/home/FooterComponent.jsx
@@ -1,18 +1,23 @@
-import logo from '../../assets/logo.png';
-
+import {Cloudinary} from "@cloudinary/url-gen";
+import {AdvancedImage , placeholder, responsive} from '@cloudinary/react';
+import {fill} from "@cloudinary/url-gen/actions/resize";
 
 const FooterComponent = () => {
+  const cld = new Cloudinary({cloud: {cloudName: 'db4mlhacn'}});
+     // Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
+     const myImage = cld.image('p76amr7g04b8ocwdgaqo'); 
+
+     // Resize to 250 x 250 pixels using the 'fill' crop mode.
+     myImage.resize(fill().width(500));
+
+     
   return (
     <footer className=" mt-20 bg-black ">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a href="#" className="flex items-center mb-4 sm:mb-0">
-            <img
-              src={logo}
-              className="h-10 mr-3"
-              alt="Dailyfoods Logo"
-            />
-          </a>
+        <a href="/" className="flex items-center mb-4 sm:mb-0 cursor-pointer w-[7em] h-[35px] lg:w-[8em] lg:h-[42px]">
+              <AdvancedImage className="w-full h-full" cldImg={myImage} plugins={[responsive(), placeholder()]}></AdvancedImage>
+            </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" className="mr-4 hover:underline md:mr-6 ">
@@ -30,7 +35,7 @@ const FooterComponent = () => {
               </a>
             </li>
             <li>
-              <a href="#" className="hover:underline">
+              <a href="mailto:animesh8492@gmail.com" className="hover:underline">
                 Contact
               </a>
             </li>
