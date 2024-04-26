1a36c1a5077a4b9623257e2a0be6500e291e23f5
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-26 12:05:23 +0530

    commit- configure skeleton

--- src/components/home/TrendingSection.jsx
+++ src/components/home/TrendingSection.jsx
@@ -1,7 +1,8 @@
-import { useContext} from 'react'
+import { useContext, useState} from 'react'
 import { Tab } from '@headlessui/react'
 import RecipeComponent from './RecipeComponent'
 import { ApiContext } from '../../context/ApiContext';
+import { MainPageSkeleton } from '../skeletons/MainPageSkeleton';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -12,20 +13,22 @@ const headings = ["Trending Recipe", "Most Searched Recipe"]
 
 const TrendingSection = () => {
     const {bulkRecipe,randomData} = useContext(ApiContext);
-    console.log(`from Trending ${randomData}`)
+    const [loading, isLoading] = useState(true)
+
+    setTimeout(() => {
+      isLoading(false)
+    }, 1000);
 
   return (
       <Tab.Group>
-      <Tab.List className="relative mx-2 lg:mx-0 mt-10 lg:p-10 flex lg:py-5 items-center lg:text-xl lg:mt-4 text-gray-50">
-      <div className="flex border-t border-gray-100 ">
-        </div>  
+      <Tab.List className="relative mx-2 lg:mx-0 mt-10 lg:p-10 flex lg:py-5 items-center lg:text-xl lg:mt-4 text-gray-50"> 
         <div className='mt-10 w-full flex justify-center'>
         {headings.map((category) => (
             <Tab
               key={category}
               className={({ selected }) =>
                 classNames(
-                  'w-1/3 rounded-lg m-2  py-4 text-md font-medium leading-5',
+                  'w-1/2 lg:w-1/3 rounded-lg m-2 py-2 lg:py-4 text-sm lg:text-md font-medium leading-5',
                   'bg-black ',
                   selected
                     ? 'bg-red-600 text-gray-100  border-0 shadow'
@@ -36,13 +39,19 @@ const TrendingSection = () => {
               {category}
             </Tab>
           ))}
-        </div>
-     
-    
+        </div> 
       </Tab.List>
       <Tab.Panels>
-        <Tab.Panel><RecipeComponent bulkRecipe={bulkRecipe}/></Tab.Panel>
-        <Tab.Panel><RecipeComponent bulkRecipe={randomData}/></Tab.Panel>
+        <Tab.Panel>
+          <div className='flex flex-wrap justify-center'>
+          {loading? <MainPageSkeleton cards={10}/> : <RecipeComponent bulkRecipe={bulkRecipe}/>}
+          </div>
+          </Tab.Panel>
+        <Tab.Panel>
+        <div className='flex flex-wrap justify-center'>
+          {loading? <MainPageSkeleton cards={10}/> : <RecipeComponent bulkRecipe={randomData}/>}
+          </div>
+          </Tab.Panel>
       </Tab.Panels>
     </Tab.Group>
   )
